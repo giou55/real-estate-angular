2a1e813d11f7bdf14f5273a1523b6bb55c7b8372
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-04-17 16:40:09 +0300

    changes

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -9,7 +9,17 @@ export class SearchFormComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  toggleAccordian(event) {
+    var element = event.target;
+    element.classList.toggle("active");
+    var panel = element.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
   }
 
 }
