eb509085c231c74ef200871c571e828fe31620f9
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-04-26 00:50:15 +0300

    create model

---
+++ src/app/models/house.model.ts
@@ -0,0 +1,14 @@
+export class House {
+      constructor(
+            public title: string,
+            public description: string,
+            public location: string,
+            public area_size: number,
+            public year_built: number,
+            public beds: number,
+            public baths: number,
+            public garages: number,
+            public features: string[],
+            public imagePath: string,
+            public price: number) { }
+}
