9b9606f4581d3a9a0f5887fa7d728ed3c612270b
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-06-25 17:48:28 +0300

    working on search form

--- src/app/components/search-form-new/search-form-new.component.ts
+++ src/app/components/search-form-new/search-form-new.component.ts
@@ -3,33 +3,42 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 declare const createCustomSelect: any;
 
 @Component({
-    selector: 'app-search-form-new',
-    templateUrl: './search-form-new.component.html',
-    styleUrls: ['./search-form-new.component.scss']
+        selector: 'app-search-form-new',
+        templateUrl: './search-form-new.component.html',
+        styleUrls: ['./search-form-new.component.scss']
 })
 export class SearchFormNewComponent implements OnInit, AfterViewInit {
 
-    constructor() { }
+        constructor() { }
 
-    ngOnInit(): void { }
+        ngOnInit(): void { }
 
-    ngAfterViewInit(): void {
-        createCustomSelect();
-    }
-
-    toggleAccordian() {
-        var i = document.getElementById("fa-symbol");
-        // var panel = element.nextElementSibling;
-        i.classList.toggle("fa-minus-square-o");
-        i.classList.toggle("fa-plus-square-o");
-        var panel = document.getElementById("panel");
-        panel.classList.toggle("o-visible");
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
+        ngAfterViewInit(): void {
+                createCustomSelect();
         }
-    }
+
+        toggleAccordian() {
+                var x = document.querySelector("p.accordion > span");
+                if (x.innerHTML === "More Filters") {
+                        x.innerHTML = "Less Filters";
+                } else {
+                        x.innerHTML = "More Filters";
+                }
+
+                var i = document.getElementById("fa-symbol");
+                // i.classList.toggle("fa-minus-square-o");
+                // i.classList.toggle("fa-plus-square-o");
+                i.classList.toggle("fa-minus");
+                i.classList.toggle("fa-plus");
+
+                var panel = document.getElementById("panel");
+                panel.classList.toggle("o-visible");
+                if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                } else {
+                        panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+        }
 
 }
 
