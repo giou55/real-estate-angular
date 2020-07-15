a8915242904308c9a37cd19f87aa6cb1125bc5a3
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-07-15 20:17:51 +0300

    working on search form

--- src/app/components/search-form-new/search-form-new.component.ts
+++ src/app/components/search-form-new/search-form-new.component.ts
@@ -1,111 +1,113 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 interface Value {
-  value: string;
-  viewValue: string;
+      value: string;
+      viewValue: string;
 }
 
 @Component({
-  selector: 'app-search-form-new',
-  templateUrl: './search-form-new.component.html',
-  styleUrls: ['./search-form-new.component.scss'],
-  encapsulation: ViewEncapsulation.None
+      selector: 'app-search-form-new',
+      templateUrl: './search-form-new.component.html',
+      styleUrls: ['./search-form-new.component.scss'],
+      encapsulation: ViewEncapsulation.None
 })
 export class SearchFormNewComponent {
-  constructor(private router: Router, private route: ActivatedRoute) { }
+      constructor(private router: Router, private route: ActivatedRoute) { }
 
-  prices: Value[] = [
-    { value: 'any', viewValue: 'Any' },
-    { value: '10000', viewValue: '10000' },
-    { value: '20000', viewValue: '20000' },
-    { value: '50000', viewValue: '50000' },
-    { value: '100000', viewValue: '100000' },
-    { value: '200000', viewValue: '200000' }
-  ];
-  beds: Value[] = [
-    { value: 'any', viewValue: 'Any' },
-    { value: '1', viewValue: '1' },
-    { value: '2', viewValue: '2' },
-    { value: '3', viewValue: '3' },
-    { value: '4', viewValue: '4' }
-  ];
-  baths: Value[] = [
-    { value: 'any', viewValue: 'Any' },
-    { value: '1', viewValue: '1' },
-    { value: '2', viewValue: '2' },
-    { value: '3', viewValue: '3' },
-    { value: '4', viewValue: '4' }
-  ];
-  area: Value[] = [
-    { value: 'any', viewValue: 'Any' },
-    { value: '150', viewValue: '150' },
-    { value: '250', viewValue: '250' },
-    { value: '350', viewValue: '350' },
-    { value: '450', viewValue: '450' }
-  ];
-  year: Value[] = [
-    { value: 'any', viewValue: 'Any' },
-    { value: '1900', viewValue: '1900' },
-    { value: '1920', viewValue: '1920' },
-    { value: '1940', viewValue: '1940' },
-    { value: '1960', viewValue: '1960' },
-    { value: '1980', viewValue: '1980' },
-    { value: '2000', viewValue: '2000' }
-  ];
+      prices: Value[] = [
+            { value: 'any', viewValue: 'Any' },
+            { value: '10000', viewValue: '10000' },
+            { value: '20000', viewValue: '20000' },
+            { value: '50000', viewValue: '50000' },
+            { value: '100000', viewValue: '100000' },
+            { value: '200000', viewValue: '200000' }
+      ];
+      beds: Value[] = [
+            { value: 'any', viewValue: 'Any' },
+            { value: '1', viewValue: '1' },
+            { value: '2', viewValue: '2' },
+            { value: '3', viewValue: '3' },
+            { value: '4', viewValue: '4' }
+      ];
+      baths: Value[] = [
+            { value: 'any', viewValue: 'Any' },
+            { value: '1', viewValue: '1' },
+            { value: '2', viewValue: '2' },
+            { value: '3', viewValue: '3' },
+            { value: '4', viewValue: '4' }
+      ];
+      area: Value[] = [
+            { value: 'any', viewValue: 'Any' },
+            { value: '150', viewValue: '150' },
+            { value: '250', viewValue: '250' },
+            { value: '350', viewValue: '350' },
+            { value: '450', viewValue: '450' }
+      ];
+      year: Value[] = [
+            { value: 'any', viewValue: 'Any' },
+            { value: '1900', viewValue: '1900' },
+            { value: '1920', viewValue: '1920' },
+            { value: '1940', viewValue: '1940' },
+            { value: '1960', viewValue: '1960' },
+            { value: '1980', viewValue: '1980' },
+            { value: '2000', viewValue: '2000' }
+      ];
 
-  selectedLocation = " ";
-  selectedMinPrice = this.prices[0].value;
-  selectedMaxPrice = this.prices[0].value;
-  selectedBeds = this.beds[0].value;
-  selectedBaths = this.baths[0].value;
-  selectedMinArea = this.area[0].value;
-  selectedMaxArea = this.area[0].value;
-  selectedMinYear = this.year[0].value;
-  selectedMaxYear = this.year[0].value;
-  emptyField = false;
+      @Input() location: string;
 
-  toggleAccordian() {
-    var x = document.querySelector("p.accordion > span");
-    if (x.innerHTML === "More Filters") {
-      x.innerHTML = "Less Filters";
-    } else {
-      x.innerHTML = "More Filters";
-    }
+      selectedLocation = "";
+      selectedMinPrice = this.prices[0].value;
+      selectedMaxPrice = this.prices[0].value;
+      selectedBeds = this.beds[0].value;
+      selectedBaths = this.baths[0].value;
+      selectedMinArea = this.area[0].value;
+      selectedMaxArea = this.area[0].value;
+      selectedMinYear = this.year[0].value;
+      selectedMaxYear = this.year[0].value;
+      emptyField = false;
 
-    var i = document.getElementById("fa-symbol");
-    // i.classList.toggle("fa-minus-square-o");
-    // i.classList.toggle("fa-plus-square-o");
-    i.classList.toggle("fa-minus");
-    i.classList.toggle("fa-plus");
+      toggleAccordian() {
+            var x = document.querySelector("p.accordion > span");
+            if (x.innerHTML === "More Filters") {
+                  x.innerHTML = "Less Filters";
+            } else {
+                  x.innerHTML = "More Filters";
+            }
 
-    var panel = document.getElementById("panel");
-    panel.classList.toggle("o-visible");
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  }
+            var i = document.getElementById("fa-symbol");
+            // i.classList.toggle("fa-minus-square-o");
+            // i.classList.toggle("fa-plus-square-o");
+            i.classList.toggle("fa-minus");
+            i.classList.toggle("fa-plus");
 
-  onSubmit(form: NgForm) {
-    if (form.value.location == "") {
-      this.emptyField = true;
-      return;
-    }
-    if (form.value.location != "") {
-      this.emptyField = false;
-      console.log(form);
-      // for (let x in form.value) {
-      //   console.log(x + "=" + form.value[x] + "<br>");
-      // }
-      this.router.navigate(['searchResults'],
-        {
-          queryParams: {
-            location_contains: form.value.location
-          }
-        });
-    }
-  }
+            var panel = document.getElementById("panel");
+            panel.classList.toggle("o-visible");
+            if (panel.style.maxHeight) {
+                  panel.style.maxHeight = null;
+            } else {
+                  panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+      }
+
+      onSubmit(form: NgForm) {
+            if (form.value.location == "") {
+                  this.emptyField = true;
+                  return;
+            }
+            if (form.value.location != "") {
+                  this.emptyField = false;
+                  console.log(form);
+                  // for (let x in form.value) {
+                  //   console.log(x + "=" + form.value[x] + "<br>");
+                  // }
+                  this.router.navigate(['searchResults'],
+                        {
+                              queryParams: {
+                                    location_contains: form.value.location
+                              }
+                        });
+            }
+      }
 }
--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -5,51 +5,55 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Property } from "../../models/property.model";
 
 @Component({
-        selector: 'app-search-results',
-        templateUrl: './search-results.component.html',
-        styleUrls: ['./search-results.component.scss']
+      selector: 'app-search-results',
+      templateUrl: './search-results.component.html',
+      styleUrls: ['./search-results.component.scss']
 })
 export class SearchResultsComponent implements OnInit {
 
-        constructor(
-                private route: ActivatedRoute,
-                private http: HttpClient
-        ) { }
+      constructor(
+            private route: ActivatedRoute,
+            private http: HttpClient
+      ) { }
 
-        results: Property[] = [];
-        id: String;
-        obj = {};
+      results: Property[] = [];
+      id: String;
+      obj = {};
+      currentLocation: String;
 
-        ngOnInit(): void {
+      ngOnInit(): void {
 
-                console.log(this.route.snapshot.queryParams.location_contains);
+            console.log(this.route.snapshot.queryParams.location_contains);
 
-                if (this.route.snapshot.queryParams.location_contains == '') {
-                        this.results = [];
-                }
+            if (this.route.snapshot.queryParams.location_contains == '') {
+                  this.results = [];
+            } else {
+                  this.currentLocation = this.route.snapshot.queryParams.location_contains;
+            }
 
-                this.route.queryParams.subscribe(
-                        (params: Params) => {
-                                console.log(params);
-                                this.obj = params;
+            this.route.queryParams.subscribe(
+                  (params: Params) => {
+                        console.log(params);
+                        this.obj = params;
 
-                                var str = "";
-                                for (var key in this.obj) {
-                                        if (str != "") {
-                                                str += "&";
-                                        }
-                                        str += key + "=" + encodeURIComponent(this.obj[key]);
-                                }
-
-                                this.http.get<Property[]>('http://localhost:1337/properties?' + str).subscribe(
-                                        results => {
-                                                console.log(results);
-                                                this.results = results;
-                                        }
-                                );
+                        var str = "";
+                        for (var key in this.obj) {
+                              if (str != "") {
+                                    str += "&";
+                              }
+                              str += key + "=" + encodeURIComponent(this.obj[key]);
                         }
-                )
-        }
+
+                        this.http.get<Property[]>('http://localhost:1337/properties?' + str).subscribe(
+                              results => {
+                                    this.currentLocation = this.route.snapshot.queryParams.location_contains;
+                                    console.log(results);
+                                    this.results = results;
+                              }
+                        );
+                  }
+            )
+      }
 
 }
 
