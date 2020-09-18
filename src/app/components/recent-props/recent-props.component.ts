1388c7869ce3e1e666e367c4596d3e92088e40c7
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-09-18 14:58:43 +0300

    working on addToFavorites function

--- src/app/components/recent-props/recent-props.component.ts
+++ src/app/components/recent-props/recent-props.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 import { Property } from "../../models/property.model";
+import { User } from '../../models/user.model';
 
 @Component({
       selector: 'app-recent-props',
@@ -16,7 +17,7 @@ export class RecentPropsComponent implements OnInit, OnDestroy {
       recentProperties: Property[] = [];
       isAuthenticated = false;
       private userSub: Subscription;
-      userEmail: string;
+      user: User = null;
 
       constructor(
             private http: HttpClient,
@@ -30,7 +31,7 @@ export class RecentPropsComponent implements OnInit, OnDestroy {
             this.userSub = this.authService.user.subscribe(user => {
                   this.isAuthenticated = !!user;
                   if (user) {
-                        this.userEmail = user.email;
+                        this.user = user;
                   }
             });
             this.http.get<Property[]>('http://localhost:1337/properties').subscribe(
@@ -40,8 +41,24 @@ export class RecentPropsComponent implements OnInit, OnDestroy {
       }
 
       addToFavorites(propID) {
+            // this.http
+            // .put<any>(
+            //       'http://localhost:1337/properties/' + propID,
+            //       {
+            //             "identifier": username,
+            //             "password": password
+            //       }
+            // )
+            // .subscribe(
+            //       properties => {
+            //             this.recentProperties = properties;
+            //       }
+            // );
+
             if (this.isAuthenticated) {
                   console.log("property ID:" + propID);
+                  console.log("user ID:" + this.user.id);
+                  console.log("user Email:" + this.user.email);
             } else {
                   console.log("You are not authenticated");
                   this.router.navigate(['login']);
