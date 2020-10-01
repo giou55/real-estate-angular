4c2850f6bdc864747bd491b0ce11c7663bb79c45
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-10-01 18:54:59 +0300

    changes

--- src/app/components/recent-props/recent-props.component.ts
+++ src/app/components/recent-props/recent-props.component.ts
@@ -4,74 +4,74 @@ import { Subscription } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 import { FavoriteHomeService } from '../../services/favoriteHome.service';
 
-import { Property } from "../../models/property.model";
+import { Property } from '../../models/property.model';
 import { User } from '../../models/user.model';
 
 @Component({
-      selector: 'app-recent-props',
-      templateUrl: './recent-props.component.html',
-      styleUrls: ['./recent-props.component.scss']
+    selector: 'app-recent-props',
+    templateUrl: './recent-props.component.html',
+    styleUrls: ['./recent-props.component.scss'],
 })
 export class RecentPropsComponent implements OnInit, OnDestroy {
-      error = null;
-      recentProperties: Property[] = [];
-      isAuthenticated = false;
-      private userSub: Subscription;
-      private favSub: Subscription;
-      user: User = null;
+    error = null;
+    recentProperties: Property[] = [];
+    isAuthenticated = false;
+    private userSub: Subscription;
+    private favSub: Subscription;
+    user: User = null;
 
-      constructor(
-            private http: HttpClient,
-            private authService: AuthService,
-            private favoriteHomeService: FavoriteHomeService
-      ) { }
+    constructor(
+        private http: HttpClient,
+        private authService: AuthService,
+        private favoriteHomeService: FavoriteHomeService
+    ) {}
 
-
-
-      ngOnInit(): void {
-            this.userSub = this.authService.user.subscribe(user => {
-                  this.isAuthenticated = !!user;
-                  if (user) {
-                        this.user = user;
-                  }
+    ngOnInit(): void {
+        this.userSub = this.authService.user.subscribe((user) => {
+            this.isAuthenticated = !!user;
+            if (user) {
+                this.user = user;
+            }
+        });
+        this.http
+            .get<Property[]>('http://localhost:1337/properties')
+            .subscribe((properties) => {
+                this.recentProperties = properties;
             });
-            this.http.get<Property[]>('http://localhost:1337/properties').subscribe(
-                  properties => {
-                        this.recentProperties = properties;
-                  });
-      }
+    }
 
-      toggleFavorite(prop: any, event: any) {
-            event.srcElement.previousSibling.style.display = "inline-block";
-            if (event.srcElement.parentElement.classList.contains("white-heart")) {
-                  this.favSub = this.favoriteHomeService.addToFavorites(prop.id, this.user)
-                        .subscribe(
-                              () => {
-                                    event.srcElement.previousSibling.style.display = "none";
-                                    event.srcElement.parentElement.classList.remove("white-heart");
-                                    event.srcElement.parentElement.classList.add("red-heart");
-                              }
-                        );
-            } else {
-                  this.favSub = this.favoriteHomeService.removeFromFavorites(prop.id, this.user)
-                        .subscribe(
-                              () => {
-                                    event.srcElement.previousSibling.style.display = "none";
-                                    event.srcElement.parentElement.classList.remove("red-heart");
-                                    event.srcElement.parentElement.classList.add("white-heart");
-                              }
-                        );
-            }
-      }
+    toggleFavorite(prop: any, event: any) {
+        event.srcElement.previousSibling.style.display = 'inline-block';
+        if (event.srcElement.parentElement.classList.contains('white-heart')) {
+            this.favSub = this.favoriteHomeService
+                .addToFavorites(prop.id, this.user)
+                .subscribe(() => {
+                    event.srcElement.previousSibling.style.display = 'none';
+                    event.srcElement.parentElement.classList.remove(
+                        'white-heart'
+                    );
+                    event.srcElement.parentElement.classList.add('red-heart');
+                });
+        } else {
+            this.favSub = this.favoriteHomeService
+                .removeFromFavorites(prop.id, this.user)
+                .subscribe(() => {
+                    event.srcElement.previousSibling.style.display = 'none';
+                    event.srcElement.parentElement.classList.remove(
+                        'red-heart'
+                    );
+                    event.srcElement.parentElement.classList.add('white-heart');
+                });
+        }
+    }
 
-      isFavorite(prop: any): boolean {
-            if (this.user) {
-                  return prop.favoriteBy.some(p => p.id == this.user.id)
-            }
-      }
+    isFavorite(prop: any): boolean {
+        if (this.user) {
+            return prop.favoriteBy.some((p) => p.id == this.user.id);
+        }
+    }
 
-      ngOnDestroy() {
-            this.userSub.unsubscribe();
-      }
-
+    ngOnDestroy() {
+        this.userSub.unsubscribe();
+    }
 }
