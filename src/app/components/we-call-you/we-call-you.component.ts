c93d9852191547ed192eebda85e7e37d14cc759a
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-10-01 16:15:49 +0300

    changes

--- src/app/components/we-call-you/we-call-you.component.ts
+++ src/app/components/we-call-you/we-call-you.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-      selector: 'app-we-call-you',
-      templateUrl: './we-call-you.component.html',
-      styleUrls: ['./we-call-you.component.scss'],
+    selector: 'app-we-call-you',
+    templateUrl: './we-call-you.component.html',
+    styleUrls: ['./we-call-you.component.scss'],
 })
 export class WeCallYouComponent {
-      checked = false;
-      indeterminate = false;
-      labelPosition: 'before' | 'after' = 'after';
-      disabled = false;
+    checked = false;
+    indeterminate = false;
+    labelPosition: 'before' | 'after' = 'after';
+    disabled = false;
 }
