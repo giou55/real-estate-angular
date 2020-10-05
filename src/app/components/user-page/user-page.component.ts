c2592d3a464e6b7f9c098403cb233b2fc575e372
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-10-05 16:19:25 +0300

    working on responsive css

--- src/app/components/user-page/user-page.component.ts
+++ src/app/components/user-page/user-page.component.ts
@@ -2,42 +2,60 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 
-import { Property } from "../../models/property.model";
+import { Property } from '../../models/property.model';
 
 import { AuthService } from '../../services/auth.service';
+import { FavoriteHomeService } from '../../services/favoriteHome.service';
 
 @Component({
-      selector: 'app-user-page',
-      templateUrl: './user-page.component.html',
-      styleUrls: ['./user-page.component.scss']
+    selector: 'app-user-page',
+    templateUrl: './user-page.component.html',
+    styleUrls: ['./user-page.component.scss'],
 })
 export class UserPageComponent implements OnInit, OnDestroy {
-      private userSub: Subscription;
-      user = {
-            username: "",
-            email: "",
-            id: null
-      };
-      properties: Property[] = [];
+    private userSub: Subscription;
+    user = {
+        username: '',
+        email: '',
+        id: null,
+    };
+    properties: Property[] = [];
+    isLoading = false;
+    private favSub: Subscription;
 
-      constructor(private authService: AuthService, private http: HttpClient) { }
+    constructor(
+        private authService: AuthService,
+        private http: HttpClient,
+        private favoriteHomeService: FavoriteHomeService
+    ) {}
 
-      ngOnInit() {
-            this.userSub = this.authService.user.subscribe(user => {
-                  if (user) {
-                        this.user.username = user.username;
-                        this.user.email = user.email;
-                        this.user.id = user.id;
-                        this.http.get<Property[]>('http://localhost:1337/properties?favoriteBy.id=' + this.user.id).subscribe(
-                              properties => {
-                                    this.properties = properties;
-                              });
-                  }
-            });
-      }
+    ngOnInit() {
+        this.isLoading = true;
+        this.userSub = this.authService.user.subscribe((user) => {
+            if (user) {
+                this.user.username = user.username;
+                this.user.email = user.email;
+                this.user.id = user.id;
+                this.http
+                    .get<Property[]>(
+                        'http://localhost:1337/properties?favoriteBy.id=' +
+                            this.user.id
+                    )
+                    .subscribe((properties) => {
+                        this.isLoading = false;
+                        this.properties = properties;
+                    });
+            }
+        });
+    }
 
-      ngOnDestroy() {
-            this.userSub.unsubscribe();
-      }
+    removeFavorite(prop) {
+        // this.favSub = this.favoriteHomeService
+        //     .removeFromFavorites(prop.propID, this.user)
+        //     .subscribe();
+    }
 
+    ngOnDestroy() {
+        this.userSub.unsubscribe();
+    }
 }
--- src/app/services/favoriteHome.service.ts
+++ src/app/services/favoriteHome.service.ts
@@ -1,63 +1,56 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { switchMap, map, tap, filter } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 
 import { User } from '../models/user.model';
 
 @Injectable({ providedIn: 'root' })
-
 export class FavoriteHomeService {
+    constructor(private http: HttpClient) {}
 
-      constructor(
-            private http: HttpClient
-      ) { }
+    addToFavorites(propID: number, user: User) {
+        return this.http
+            .get<any>('http://localhost:1337/properties/' + propID)
+            .pipe(
+                switchMap((resData) =>
+                    this.http.put<any>(
+                        'http://localhost:1337/properties/' + propID,
+                        {
+                            favoriteBy: [
+                                ...resData.favoriteBy,
+                                {
+                                    id: user.id,
+                                    username: user.username,
+                                    email: user.email,
+                                    provider: 'local',
+                                    confirmed: true,
+                                    blocked: false,
+                                    role: 1,
+                                    created_by: 1,
+                                    updated_by: 1,
+                                    created_at: '2020-09-16T11:18:28.000Z',
+                                    updated_at: '2020-09-18T11:17:21.000Z',
+                                },
+                            ],
+                        }
+                    )
+                )
+            );
+    }
 
-      addToFavorites(propID: number, user: User) {
-            return this.http
-                  .get<any>('http://localhost:1337/properties/' + propID)
-                  .pipe(
-                        switchMap(
-                              (resData => this.http
-                                    .put<any>('http://localhost:1337/properties/' + propID,
-                                          {
-                                                "favoriteBy": [
-                                                      ...resData.favoriteBy,
-                                                      {
-                                                            "id": user.id,
-                                                            "username": user.username,
-                                                            "email": user.email,
-                                                            "provider": "local",
-                                                            "confirmed": true,
-                                                            "blocked": false,
-                                                            "role": 1,
-                                                            "created_by": 1,
-                                                            "updated_by": 1,
-                                                            "created_at": "2020-09-16T11:18:28.000Z",
-                                                            "updated_at": "2020-09-18T11:17:21.000Z"
-                                                      }
-                                                ]
-                                          }
-                                    )
-                              )
-                        )
-                  );
-      }
-
-      removeFromFavorites(propID: number, user: User) {
-            return this.http
-                  .get<any>('http://localhost:1337/properties/' + propID)
-                  .pipe(
-                        map(
-                              data => data.favoriteBy.filter(x => x.id !== user.id)
-                        ),
-                        switchMap(
-                              newDataArray => this.http
-                                    .put<any>('http://localhost:1337/properties/' + propID,
-                                          {
-                                                "favoriteBy": newDataArray
-                                          }
-                                    )
-                        )
-                  );
-      }
+    removeFromFavorites(propID: number, user: User) {
+        return this.http
+            .get<any>('http://localhost:1337/properties/' + propID)
+            .pipe(
+                map((data) => data.favoriteBy.filter((x) => x.id !== user.id)),
+                switchMap((newDataArray) =>
+                    this.http.put<any>(
+                        'http://localhost:1337/properties/' + propID,
+                        {
+                            favoriteBy: newDataArray,
+                        }
+                    )
+                )
+            );
+    }
 }
