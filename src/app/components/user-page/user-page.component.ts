07f17a03a9e03f95681d41225af9421783b7e13a
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-10-06 16:21:07 +0300

    changes

--- src/app/components/user-page/user-page.component.ts
+++ src/app/components/user-page/user-page.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 
 import { Property } from '../../models/property.model';
+import { User } from '../../models/user.model';
 
 import { AuthService } from '../../services/auth.service';
 import { FavoriteHomeService } from '../../services/favoriteHome.service';
@@ -14,11 +15,7 @@ import { FavoriteHomeService } from '../../services/favoriteHome.service';
 })
 export class UserPageComponent implements OnInit, OnDestroy {
     private userSub: Subscription;
-    user = {
-        username: '',
-        email: '',
-        id: null,
-    };
+    user: User = null;
     properties: Property[] = [];
     isLoading = false;
     private favSub: Subscription;
@@ -33,9 +30,7 @@ export class UserPageComponent implements OnInit, OnDestroy {
         this.isLoading = true;
         this.userSub = this.authService.user.subscribe((user) => {
             if (user) {
-                this.user.username = user.username;
-                this.user.email = user.email;
-                this.user.id = user.id;
+                this.user = user;
                 this.http
                     .get<Property[]>(
                         'http://localhost:1337/properties?favoriteBy.id=' +
@@ -49,10 +44,10 @@ export class UserPageComponent implements OnInit, OnDestroy {
         });
     }
 
-    removeFavorite(prop) {
-        // this.favSub = this.favoriteHomeService
-        //     .removeFromFavorites(prop.propID, this.user)
-        //     .subscribe();
+    removeFavorite(propID) {
+        this.favoriteHomeService
+            .removeFromFavorites(propID, this.user)
+            .subscribe(() => console.log('favorite home deleted'));
     }
 
     ngOnDestroy() {
