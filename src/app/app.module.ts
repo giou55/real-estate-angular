e7653e8b76654ce24ef5c00c69b03c59408865a1
Giorgos Giourmetakis <giorgosggg@gmail.com>
2020-11-19 22:50:34 +0200

    a little change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
-import { WeCallYouComponent } from './components/we-call-you/we-call-you.component';
 import { RecentPropertiesComponent } from './components/recent-properties/recent-properties.component';
 import { PropertyComponent } from './components/property/property.component';
 import { HomeComponent } from './components/home/home.component';
@@ -43,7 +42,6 @@ import { SearchFormMinimalComponent } from './components/search-form-minimal/sea
     declarations: [
         AppComponent,
         TopbarComponent,
-        WeCallYouComponent,
         RecentPropertiesComponent,
         PropertyComponent,
         HomeComponent,
